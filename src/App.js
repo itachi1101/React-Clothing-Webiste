f289a433b638cd212805f9186a035fa7ac4b0b47
Aviral <aviral1101@gmail.com>
2021-10-22 17:07:19 +0530

    header,sign-in-out page and sign-in component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./component/Header/header.component";
+import SignInandSignOutPage from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={SignInandSignOutPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sign-in-and-sign-out.styles.scss";
+import SignIn from "../../component/sign-in/sign-in.component";
+function SignInandSignOutPage() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
+export default SignInandSignOutPage;
