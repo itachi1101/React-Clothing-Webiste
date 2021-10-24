83a3bbef36b011c7a73fa358f0e5c2117e2f5831
Aviral <aviral1101@gmail.com>
2021-10-24 14:57:55 +0530

    sign-in-and-sign-up page made

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./component/Header/header.component";
 import SignInandSignOutPage from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 class App extends React.Component {
@@ -15,9 +15,21 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    console.log(this.state.currentUser);
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+      else{
+        this.setState({currentUser:userAuth})
+      }
     });
   }
   componentWillUnmount() {
--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./sign-in-and-sign-out.styles.scss";
 import SignIn from "../../component/sign-in/sign-in.component";
+import SignUp from "../../component/sign-up/sign-up.component";
 function SignInandSignOutPage() {
   return (
-    <div>
+    <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
